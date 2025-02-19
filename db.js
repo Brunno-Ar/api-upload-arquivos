6da01d286f1605ce0266ffa55fc8c8413c94791e
Bruno Araujo <Brunnoaraujoc@gmail.com>
2025-02-19 17:04:03 -0300

    fix:alterando banco de dados

--- db.js
+++ db.js
@@ -1,12 +1,15 @@
 const { Pool } = require("pg");
-require("dotenv").config();
+
+// Carrega o .env apenas em ambiente de desenvolvimento
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false, // Necessário para conexões seguras
+  },
 });
 
 module.exports = pool;
